298cec874f442f60fd12cb7ee65d66d6feceb703
Manuel Duarte <manuel.duarte@sonorasoft.com>
2022-08-13 11:17:58 -0700

    refactorized NewAdModal component

--- src/pages/myAccount/components/NewAdModal/NewAdModal.tsx
+++ src/pages/myAccount/components/NewAdModal/NewAdModal.tsx
@@ -1,144 +1,142 @@
-import "./NewAdModal.scss";
-import Modal from "react-bootstrap/Modal";
-import { Brands, years } from "../../../../functions/Constantes";
-import { useEffect, useState } from "react";
-import firebase from "firebase/compat/app";
-import { v4 as uuidv4 } from 'uuid';
 import { addDoc, collection } from "firebase/firestore";
+import {
+    getDownloadURL,
+    getStorage,
+    ref,
+    uploadBytesResumable
+} from "firebase/storage";
+import { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import { v4 as uuidv4 } from "uuid";
 import { db } from "../../../../config/firebase";
-import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
 import { useAuth } from "../../../../Context/UserContext";
-
+import { Brands, years } from "../../../../functions/Constantes";
+import "./NewAdModal.scss";
 
 function NewAdModal({ show, handleClose }: any) {
+    interface Ad {
+        brand: string;
+        model: string;
+        year: number;
+        transmision: string;
+        km: number;
+        price: number;
+        description: string;
+        images: any;
+        state: any;
+        color: string;
+        carType: string;
+        motor: string;
+        vestidure: string;
+    }
 
-    const [brand, setBrand] = useState("none");
-    const [model, setModel] = useState("");
-    const [year, setYear] = useState(0);
-    const [transmision, setTransmision] = useState("none");
-    const [km, setKm] = useState<number>(0);
-    const [price, setPrice] = useState<string>("");
-    const [description, setDescription] = useState("");
-    const [images, setImages] = useState<Array<any>>([]);
-    const [state, setState] = useState<string>("none");
+    const initialAdState: Ad = {
+        brand: "",
+        model: "",
+        year: 0,
+        transmision: "",
+        km: 0,
+        price: 0,
+        description: "",
+        images: [],
+        state: "",
+        color: "",
+        carType: "",
+        motor: "",
+        vestidure: "",
+    };
+
+    const [ad, setAd] = useState<Ad>({...initialAdState});
+
     const user = useAuth().user;
-
     const [loading, setLoading] = useState<boolean>(false);
 
     const handleImages = (e: any) => {
-
         let filesD = e.files;
 
-        setImages([...images, ...Array.from(filesD)]);
-    }
-
+        setAd({ ...ad, images: [...ad.images, ...Array.from(filesD)] });
+    };
 
     const createAd = async () => {
         setLoading(true);
 
-
         let finalArrayFiles = new Array();
 
-        if (images.length > 0) {
-            for (let index = 0; index < images.length; index++) {
-                if (images[index].imageUrl === undefined) {
-
+        if (ad.images.length > 0) {
+            for (let index = 0; index < ad.images.length; index++) {
+                if (ad.images[index].imageUrl === undefined) {
                     const storage = getStorage();
-                    const storageRef = ref(storage, 'cars/' + uuidv4() + "/" + images[index].name);
+                    const storageRef = ref(
+                        storage,
+                        "cars/" + uuidv4() + "/" + ad.images[index].name
+                    );
 
                     // Create file metadata including the content type
                     /** @type {any} */
                     const metadata = {
-                        contentType: images[index].type,
+                        contentType: ad.images[index].type,
                     };
 
                     // Upload the file and metadata
-                    const uploadTask = await uploadBytesResumable(storageRef, images[index], metadata);
+                    const uploadTask = await uploadBytesResumable(
+                        storageRef,
+                        ad.images[index],
+                        metadata
+                    );
 
                     await getDownloadURL(uploadTask.ref).then((downloadURL) => {
                         const data = {
-                            name: images[index].name,
-                            imageUrl: downloadURL
-                        }
+                            name: ad.images[index].name,
+                            imageUrl: downloadURL,
+                        };
 
                         finalArrayFiles.push(data);
                     });
-
                 }
 
-                if (index === images.length - 1) {
-
-
-                    let pri = price.replace(",", "").replace(",", "");
-
+                if (index === ad.images.length - 1) {
+                    // let pri = price.replace(",", "").replace(",", "");
 
                     const docRef = await addDoc(collection(db, "ads"), {
-                        brand: brand,
-                        model: model,
-                        year: year,
-                        transmision: transmision,
-                        km: km,
-                        description: description,
-                        images: finalArrayFiles,
-                        state: state,
-                        price: parseInt(pri),
-                        userId: user.id
+                        ...ad,
+                        userId: user.id,
                     }).then(() => {
-                        setBrand("none");
-                        setModel("");
-                        setTransmision("none");
-                        setKm(0);
-                        setYear(0);
-                        setImages([]);
-                        setState("none");
+                        setAd({...initialAdState})
                         setLoading(false);
-                        setPrice("")
                         handleClose();
                     });
                 }
             }
         } else {
-            alert("Favor de agregar imagenes")
+            alert("Favor de agregar imagenes");
         }
-
-    }
+    };
 
     const close = () => {
-        setBrand("none");
-        setModel("");
-        setTransmision("none");
-        setKm(0);
-        setYear(0);
-        setImages([]);
-        setState("none");
-        setPrice("")
+        setAd({...initialAdState})
         handleClose();
-    }
+    };
 
-    const handleInputPrice = (e: any) => {
-
-        let s = e.target.value;
+    const formatPrice = (price: number) => {
+        let s = price.toString();
         let n = "";
 
         for (let index = 0; index < s.length; index++) {
             if (isNumber(s[index])) {
                 n = n + s[index];
             }
-
         }
 
-        n = n.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
-
-        setPrice(n)
-
-    }
+        n = n.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+        return n;
+    };
 
     function isNumber(char: any) {
-        if (typeof char !== 'string') {
+        if (typeof char !== "string") {
             return false;
         }
 
-        if (char.trim() === '') {
+        if (char.trim() === "") {
             return false;
         }
 
@@ -155,12 +153,19 @@ function NewAdModal({ show, handleClose }: any) {
                     <div className="form-row">
                         <h3>Marca</h3>
                         <div className="input-container">
-                            <select name="brands" id="brands" value={brand} onChange={(e) => setBrand(e.target.value)}>
+                            <select
+                                name="brands"
+                                id="brands"
+                                value={ad.brand}
+                                onChange={(e) => setAd({...ad, brand: e.target.value})}
+                            >
                                 <option value="none">Selecciona una marca</option>
                                 {Brands.map((brand, index) => {
                                     return (
-                                        <option value={brand} key={"brand-" + index} >{brand}</option>
-                                    )
+                                        <option value={brand} key={"brand-" + index}>
+                                            {brand}
+                                        </option>
+                                    );
                                 })}
                             </select>
                         </div>
@@ -168,18 +173,33 @@ function NewAdModal({ show, handleClose }: any) {
                     <div className="form-row">
                         <h3>Modelo</h3>
                         <div className="input-container">
-                            <input type="text" name="model" id="model" value={model} onChange={(e) => setModel(e.target.value)} />
+                            <input
+                                type="text"
+                                name="model"
+                                id="model"
+                                value={ad.model}
+                                onChange={(e) => setAd({ ...ad, model: e.target.value })}
+                            />
                         </div>
                     </div>
                     <div className="form-row">
                         <h3>Año</h3>
                         <div className="input-container">
-                            <select name="brands" id="brands" value={year} onChange={(e) => setYear(parseInt(e.target.value))}>
+                            <select
+                                name="brands"
+                                id="brands"
+                                value={ad.year}
+                                onChange={(e) =>
+                                    setAd({ ...ad, year: parseInt(e.target.value) })
+                                }
+                            >
                                 <option value="0">Selecciona un año</option>
                                 {years.map((year, index) => {
                                     return (
-                                        <option value={year} key={"years-" + index} >{year}</option>
-                                    )
+                                        <option value={year} key={"years-" + index}>
+                                            {year}
+                                        </option>
+                                    );
                                 })}
                             </select>
                         </div>
@@ -187,7 +207,12 @@ function NewAdModal({ show, handleClose }: any) {
                     <div className="form-row">
                         <h3>Transmisión</h3>
                         <div className="input-container">
-                            <select name="transmision" id="transmision" value={transmision} onChange={(e) => setTransmision(e.target.value)}>
+                            <select
+                                name="transmision"
+                                id="transmision"
+                                value={ad.transmision}
+                                onChange={(e) => setAd({ ...ad, transmision: e.target.value })}
+                            >
                                 <option value="none">Selecciona una opción</option>
                                 <option value="automatica">Automática</option>
                                 <option value="estandar">Estándar</option>
@@ -195,9 +220,14 @@ function NewAdModal({ show, handleClose }: any) {
                         </div>
                     </div>
                     <div className="form-row">
-                        <h3>Transmisión</h3>
+                        <h3>Estado de Auto</h3>
                         <div className="input-container">
-                            <select name="state" id="state" value={state} onChange={(e) => setState(e.target.value)}>
+                            <select
+                                name="state"
+                                id="state"
+                                value={ad.state}
+                                onChange={(e) => setAd({ ...ad, state: e.target.value })}
+                            >
                                 <option value="none">Selecciona una opción</option>
                                 <option value="nuevo">Nuevo</option>
                                 <option value="seminuevo">Seminuevo</option>
@@ -207,33 +237,63 @@ function NewAdModal({ show, handleClose }: any) {
                     <div className="form-row">
                         <h3>Kilometraje</h3>
                         <div className="input-container">
-                            <input type="number" name="km" id="km" value={km} onChange={(e) => setKm(parseInt(e.target.value))} />
+                            <input
+                                type="number"
+                                name="km"
+                                id="km"
+                                value={ad.km}
+                                onChange={(e) => setAd({ ...ad, km: parseInt(e.target.value) })}
+                            />
                         </div>
                     </div>
                     <div className="form-row">
                         <h3>Precio</h3>
                         <div className="input-container">
-                            <input type="text" name="price" id="price" maxLength={11} value={price} onChange={(e) => handleInputPrice(e)} />
+                            <input
+                                type="text"
+                                name="price"
+                                id="price"
+                                maxLength={11}
+                                value={ad.price}
+                                onChange={(e) =>
+                                    setAd({ ...ad, price: parseInt(e.target.value) })
+                                }
+                            />
                         </div>
                     </div>
                     <div className="form-row">
                         <h3>Descripción</h3>
                         <div className="input-container">
-                            <textarea name="description" id="description" rows={6} value={description} onChange={(e) => setDescription(e.target.value)} />
+                            <textarea
+                                name="description"
+                                id="description"
+                                rows={6}
+                                value={ad.description}
+                                onChange={(e) => setAd({ ...ad, description: e.target.value })}
+                            />
                         </div>
                     </div>
                     <div className="form-row">
                         <h3>Imágenes</h3>
                         <div className="input-container">
-                            <input type="file" multiple accept="image/*" onChange={(e) => handleImages(e.target)} />
+                            <input
+                                type="file"
+                                multiple
+                                accept="image/*"
+                                onChange={(e) => handleImages(e.target)}
+                            />
                         </div>
                         <div>
-                            {images.length > 0 && (
+                            {ad.images.length > 0 && (
                                 <>
-                                    {images.map((image, index) => {
+                                    {ad.images.map((image: any, index: number) => {
                                         return (
-                                            <img key={"previewImage-" + index} src={URL.createObjectURL(image)} alt="" />
-                                        )
+                                            <img
+                                                key={"previewImage-" + index}
+                                                src={URL.createObjectURL(image)}
+                                                alt=""
+                                            />
+                                        );
                                     })}
                                 </>
                             )}
